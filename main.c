e820eeace158be89707d89a5fd19acf30bd58249
Ege Güneş <egegunes@gmail.com>
2020-02-02 15:05:40 +0300

    wip: parse http request

--- main.c
+++ main.c
@@ -1,14 +1,24 @@
 #include <arpa/inet.h>
 #include <errno.h>
 #include <netinet/ip.h>
+#include <regex.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
 
+#define CR          (u_char)'\r'
+#define LF          (u_char)'\n'
+#define NULLCHAR    (u_char)'\0'
 #define LISTEN_PORT 6666
 #define REQ_SIZE 80000
+
+struct http_request_s {
+  char *method;
+  char *path;
+  char *http_version;
+};
 
 ssize_t read_request(int fd, void *buffer, size_t n) {
   ssize_t n_read;
@@ -36,15 +46,58 @@ ssize_t read_request(int fd, void *buffer, size_t n) {
       *buf++ = ch;
     }
 
-    if (ch == '\r' && lch == '\n') {
+    if (ch == CR && lch == LF) {
       break;
     }
 
     lch = ch;
   }
 
-  *buf = '\0';
+  *buf = NULLCHAR;
   return t_read;
+}
+
+int parse_request(char *req) {
+  size_t max_groups = 4;
+
+  int           ret;
+  unsigned int  req_line_offset;
+  regex_t       regex;
+  regmatch_t    groups[max_groups];
+
+  struct http_request_s request;
+
+  ret = regcomp(
+      &regex,
+      "([A-Z]+)[[:blank:]]([\\/a-zA-Z0-9.]+)[[:blank:]]HTTP\\/([0-9.]+)",
+      REG_EXTENDED);
+  if (ret != 0) {
+    return ret;
+  }
+
+  ret = regexec(&regex, req, max_groups, groups, 0);
+  if (ret != 0) {
+    return ret;
+  }
+
+  req_line_offset = groups[0].rm_eo;
+  printf("Request line offset: %d\n\n", req_line_offset);
+
+  request.method = req + groups[1].rm_so;
+  request.method[groups[1].rm_eo] = NULLCHAR;
+  printf("Method: %s\n\n", request.method);
+
+  request.path = req + groups[2].rm_so;
+  request.path[groups[2].rm_eo] = NULLCHAR;
+  printf("Path: %s\n\n", request.path);
+
+  request.http_version = req + groups[3].rm_so;
+  request.http_version[groups[3].rm_eo] = NULLCHAR;
+  printf("HTTP Version: %s\n\n", request.http_version);
+
+  regfree(&regex);
+
+  return ret;
 }
 
 ssize_t write_response(int fd) {
@@ -104,6 +157,13 @@ int main() {
   }
 
   printf("%s\n", req);
+
+  int regex_ret = parse_request(req);
+  if (regex_ret == 0) {
+    printf("Match!\n");
+  } else if (regex_ret == REG_NOMATCH) {
+    printf("No match!\n");
+  }
 
   ssize_t n_write = write_response(cfd);
   if (n_write < 0) {
